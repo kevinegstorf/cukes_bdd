a756792697f5a866a0fa62053c5aac6d8c79dac0
kevinegstorf <kevin.egstorf@gmail.com>
2015-09-28 22:25:16 +0200

    added more tests and updated rails files

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'capybara'
+gem 'simple_form'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -37,6 +39,7 @@ group :test do
   gem 'cucumber-rails', :require => false
   # database_cleaner is not required, but highly recommended
   gem 'database_cleaner'
+  gem 'minitest'
 end
 
 group :development, :test do
--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,19 +1,15 @@
 Given(/^I follow "(.*?)"$/) do |link_name|
-  pending # express the regexp above with the code you wish you had
+  click_link link_name
 end
 
 When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  fill_in field, :with => 'value'
 end
 
 When(/^I press "(.*?)"$/) do |button_name|
-  pending # express the regexp above with the code you wish you had
+  click_button button_name
 end
 
 Then(/^I should see "(.*?)"$/) do |content|
-  pending # express the regexp above with the code you wish you had
+  page.has_content?('content') 
 end
